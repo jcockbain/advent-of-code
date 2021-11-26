fbb58631b0bf5fc2a6956f4037ca59a0915f1c88
James <jcockbain96@gmail.com>
2021-11-26 22:18:23 +0000

    update template

--- template/main.go
+++ template/main.go
@@ -2,17 +2,31 @@ package main
 
 import (
 	input "aoc2021/inpututils"
+	"os"
+	"time"
 
 	"fmt"
-	"strconv"
 )
 
 func main() {
-	fmt.Println("--- Part One ---")
-	fmt.Println(Part1("input.txt"))
+	start := time.Now()
+	var input string
+	if len(os.Args) > 1 {
+		input = os.Args[1]
+	} else {
+		input = "input.txt"
+	}
 
+	fmt.Println("--- Part One ---")
+	fmt.Println(Part1(input))
+	elapsed := time.Since(start)
+	fmt.Printf("%s took %s seconds \n", "Part 1", elapsed)
+
+	start = time.Now()
 	fmt.Println("--- Part Two ---")
-	fmt.Println(Part2("input.txt"))
+	fmt.Println(Part2(input))
+	elapsed = time.Since(start)
+	fmt.Printf("%s took %s seconds \n", "Part 2", elapsed)
 }
 
 func Part1(filename string) int {
@@ -26,36 +40,4 @@ func Part1(filename string) int {
 
 func Part2(filename string) int {
 	return 12
-}
-
-func check(err error) {
-	if err != nil {
-		panic(err)
-	}
-}
-
-func toInt(s string) int {
-	i, err := strconv.Atoi(s)
-	check(err)
-	return i
-}
-
-func min(a []int) int {
-	m := a[0]
-	for _, v := range a {
-		if v < m {
-			m = v
-		}
-	}
-	return m
-}
-
-func max(a []int) int {
-	m := a[0]
-	for _, v := range a {
-		if v > m {
-			m = v
-		}
-	}
-	return m
 }
