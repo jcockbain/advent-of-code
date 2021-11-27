67047da0ded843499d09bfb1c75e5cdd9d2858cc
James <jcockbain96@gmail.com>
2021-11-27 17:53:46 +0000

    tidy script

--- new_day.sh
+++ new_day.sh
@@ -1,37 +1,25 @@
-#! /bin/bash
+#!/bin/bash
 
 YEAR=$1
 DAY=$2
 
 echo "Creating Boilerplate for year ${YEAR} day ${DAY}"
+day_length=${#DAY}
 
-# STRLENGTH=$(echo -n $DAY | wc -m)
-# echo $STRLENGTH
-if [[ ${#DAY} -ge 1 ]]
-then
-    DISPLAY_DAY="0${DAY}"
-else
-    DISPLAY_DAY=$DAY
+if [ $day_length == 1 ]
+then DISPLAY_DAY="0${DAY}"
+else DISPLAY_DAY=$DAY
 fi
-
-echo $DISPLAY_DAY
 
 GOROOT=day${DISPLAY_DAY}
 cp -n -R template $GOROOT
-
-# copy input
 
 INPUT_URL="https://adventofcode.com/$YEAR/day/$DAY/input"
 TEMP_INPUT="temp-input.txt"
 
 # "session cookie" must be given as input is per-user...
 # this can be grabbed from the URL of any input page online e.g https://adventofcode.com/2019/day/2/input
-# I have this set as an environment variable here
 
 curl "${INPUT_URL}" -H "cookie: session=${AOC_COOKIE}" -o "${TEMP_INPUT}" 2>/dev/null
-
 cp ${TEMP_INPUT} ${GOROOT}/input.txt
-
-# clean up
-
 rm ${TEMP_INPUT}
