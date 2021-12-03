17b39b2ce0eb43ad5c2f5503e96db86b5d4b192a
jcockbain <jcockbain@thoughtmachine.net>
2021-12-03 11:44:52 +0000

    tidy day03

--- day03/main.go
+++ day03/main.go
@@ -26,23 +26,17 @@ func main() {
 func Part1(filename string) int {
 	lines := input.ReadLines(filename)
 	ones := map[int]int{}
+	gammaRate, epsilonRate := 0, 0
 	for _, line := range lines {
 		for pos := len(line) - 1; pos >= 0; pos -= 1 {
 			c := string(line[pos])
-			val, err := strconv.Atoi(c)
-			if err != nil {
-				panic(err)
-			}
-			if val == 1 {
+			if getInt(c) == 1 {
 				ones[len(line)-1-pos] += 1
 			}
 		}
 	}
-
-	length := len(lines)
-	gammaRate, epsilonRate := 0, 0
 	for pos, count := range ones {
-		if count > (length - count) {
+		if count > (len(lines) - count) {
 			gammaRate += 1 << pos
 		} else {
 			epsilonRate += 1 << pos
@@ -54,33 +48,25 @@ func Part1(filename string) int {
 
 func Part2(filename string) int {
 	lines := input.ReadLines(filename)
-	oxygenRating, co2Rating := "", ""
+	oxygenRating, co2Rating := 0, 0
 	for b := 0; b < len(lines[0]); b++ {
 		newLines := []string{}
 		ones, mostCommon := 0, 0
 		for _, l := range lines {
-			c := l[b]
-			v, err := strconv.Atoi(string(c))
-			if err != nil {
-				panic(err)
-			}
-			ones += v
+			c := string(l[b])
+			ones += getInt(c)
 		}
 		if ones >= len(lines)-ones {
 			mostCommon += 1
 		}
 		for _, l := range lines {
-			c := l[b]
-			v, err := strconv.Atoi(string(c))
-			if err != nil {
-				panic(err)
-			}
-			if v == mostCommon {
+			c := string(l[b])
+			if getInt(c) == mostCommon {
 				newLines = append(newLines, l)
 			}
 		}
 		if len(newLines) == 1 {
-			oxygenRating = newLines[0]
+			oxygenRating = convertBinaryToDecimal(getInt(newLines[0]))
 		}
 		lines = newLines
 	}
@@ -90,34 +76,24 @@ func Part2(filename string) int {
 		newLines := []string{}
 		ones, leastCommon := 0, 1
 		for _, l := range lines2 {
-			c := l[b]
-			v, err := strconv.Atoi(string(c))
-			if err != nil {
-				panic(err)
-			}
-			ones += v
+			c := string(l[b])
+			ones += getInt(c)
 		}
 		if ones >= len(lines2)-ones {
 			leastCommon -= 1
 		}
 		for _, l := range lines2 {
-			c := l[b]
-			v, err := strconv.Atoi(string(c))
-			if err != nil {
-				panic(err)
-			}
-			if v == leastCommon {
+			c := string(l[b])
+			if getInt(c) == leastCommon {
 				newLines = append(newLines, l)
 			}
 		}
 		if len(newLines) == 1 {
-			co2Rating = newLines[0]
+			co2Rating = convertBinaryToDecimal(getInt(newLines[0]))
 		}
 		lines2 = newLines
 	}
-	oxygenVal := convertBinaryToDecimal(getInt(oxygenRating))
-	co2Val := convertBinaryToDecimal(getInt(co2Rating))
-	return oxygenVal * co2Val
+	return oxygenRating * co2Rating
 }
 
 func getInt(s string) int {
