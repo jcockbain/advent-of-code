e4fc8de7a37813a46414bca0cae2a13cac66ca0a
jcockbain <jcockbain96@gmail.com>
2021-12-11 21:32:45 +0000

    update bemchmark script

--- benchmark.sh
+++ benchmark.sh
@@ -2,7 +2,14 @@
 
 for i in $(seq -f "%02g" 1 25)
 do  
-    echo "benchmarking day ${i}"
-    cd "day${i}" && go test --bench=BenchmarkMain | grep "BenchmarkMain-" | awk '{print $3}' > ../benchmarks/results/${i}.txt
-    cd ..
+    if [ -d "day${i}" ]
+    then
+        echo "benchmarking day ${i}"
+        cd "day${i}" && go test --bench=BenchmarkMain | grep "BenchmarkMain-" | awk '{print $3}' > ../benchmarks/results/${i}.txt
+        cd ..
+    fi
 done
+
+cd benchmarks || exit
+go run benchmarks.go
+cd ..
