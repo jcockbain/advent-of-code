939c4cb4ff644b5db9ef0a4a234c231522e4dcde
jcockbain <jcockbain96@gmail.com>
2021-12-12 16:42:37 +0000

    add day12 test

--- day12/main_test.go
+++ day12/main_test.go
@@ -2,7 +2,16 @@ package main
 
 import (
 	"testing"
+	"github.com/stretchr/testify/assert"
 )
+
+func TestPart1(t *testing.T){
+	assert.Equal(t, 5756, part1())
+}
+
+func TestPart2(t *testing.T){
+	assert.Equal(t, 144603, part2())
+}
 
 func BenchmarkMain(b *testing.B) {
 	benchmark = true
