08e3cee5dbb982ce8c1e75a26cfd722350584566
jcockbain <jcockbain96@gmail.com>
2021-12-12 16:39:52 +0000

    improve benchmark script

--- benchmark.sh
+++ benchmark.sh
@@ -1,14 +1,25 @@
 #!/bin/bash
 
-for i in $(seq -f "%02g" 1 25)
-do  
-    if [ -d "day${i}" ]
-    then
-        echo "benchmarking day ${i}"
-        cd "day${i}" && go test --bench=BenchmarkMain | grep "BenchmarkMain-" | awk '{print $3}' > ../benchmarks/results/${i}.txt
-        cd ..
-    fi
-done
+function bench {
+    echo "benchmarking day $1"
+    cd "day$1" && go test --bench=BenchmarkMain | grep "BenchmarkMain-" | awk '{print $3}' > ../benchmarks/results/$1.txt
+    cd ..
+}
+
+if [ $# -eq 0 ]
+then
+    for i in $(seq -f "%02g" 1 25)
+    do  
+        if [ -d "day${i}" ]
+        then
+        bench $i
+        fi
+    done
+else
+    bench $1
+fi
+
+
 
 cd benchmarks || exit
 go run benchmarks.go
