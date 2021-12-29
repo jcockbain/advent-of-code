8b6eab5a0eb27c767ff0c5bdaba33214ecfeeabd
jcockbain <jcockbain96@gmail.com>
2021-12-29 14:32:18 +0000

    add Dockerfile

---
+++ Dockerfile
@@ -0,0 +1,12 @@
+FROM golang:1.18beta1-bullseye
+
+WORKDIR /app
+
+ENV USER=gopher
+
+RUN useradd --create-home $USER \
+  && chown -R $USER:$USER /app
+
+USER $USER
+
+COPY --chown=$USER:$USER . .
---
+++ test.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+d=${1:-$DAY}
+day=day${d}
+
+if [ -z $day ]; then
+  echo "Set \$DAY or pass day directory as an arg to test single file"
+  # test all
+  go test ./...
+elif [ ! -d $day ]; then
+  echo "$day is not a directory!"
+  exit 1
+else
+  # test individual day
+  cd $day && go test
+fi
