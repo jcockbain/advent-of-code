6696405e86e6065c089beb8a69d3979773cbf826
jcockbain <jcockbain@thoughtmachine.net>
2022-12-08 19:11:35 +0000

    day 08

--- 2022/day08/main.go
+++ 2022/day08/main.go
@@ -13,7 +13,7 @@ var (
 	benchmark = false
 )
 
-//go:embed test1.txt
+//go:embed input.txt
 var input string
 
 func main() {
@@ -27,61 +27,82 @@ func main() {
 }
 
 type pos struct {
-	x, y int
+	y, x int
 }
 
 func part1() int {
 	lines := utils.GetLines(input)
 	width := len(lines[0])
 	height := len(lines)
-
 	treeMap := map[pos]int{}
+	seen := map[pos]struct{}{}
 
-	bounds := map[pos][]int{}
-
-	sum := 0
 	for y, line := range lines {
 		for x, c := range line {
 			treeMap[pos{y, x}] = toInt(string(c))
-			bounds[pos{y, x}] = []int{0, 0, 0, 0}
 		}
 	}
 
+	// look down
+	for x := 0; x < width; x++ {
+		currentMax := -1
+		for y := 0; y < height; y++ {
+			p := pos{y, x}
+			v := treeMap[p]
+			if v > currentMax {
+				seen[p] = struct{}{}
+			}
+			currentMax = max(currentMax, v)
+		}
+	}
+
+	// look up
+	for x := 0; x < width; x++ {
+		currentMax := -1
+		for y := height - 1; y >= 0; y-- {
+			p := pos{y, x}
+			v := treeMap[p]
+			if v > currentMax {
+				seen[p] = struct{}{}
+			}
+			currentMax = max(currentMax, v)
+		}
+	}
+
+	// look right
 	for y := 0; y < height; y++ {
+		currentMax := -1
 		for x := 0; x < width; x++ {
-			t := treeMap[pos{y, x}]
-			if y > 0 {
-				bounds[pos{y, x}][0] = bounds[pos{y - 1, x}][0] + t
+			p := pos{y, x}
+			v := treeMap[p]
+			if v > currentMax {
+				seen[p] = struct{}{}
 			}
-			if x > 0 {
-				bounds[pos{y, x}][1] = bounds[pos{y, x - 1}][1] + t
-			}
+			currentMax = max(currentMax, v)
 		}
 	}
 
-	for y := height - 1; y >= 0; y-- {
+	// look left
+	for y := 0; y < height; y++ {
+		currentMax := -1
 		for x := width - 1; x >= 0; x-- {
-			t := treeMap[pos{y, x}]
-			if y < height-1 {
-				bounds[pos{y, x}][2] = bounds[pos{y + 1, x}][2] + t
+			p := pos{y, x}
+			v := treeMap[p]
+			if v > currentMax {
+				seen[p] = struct{}{}
 			}
-			if x < width-1 {
-				bounds[pos{y, x + 1}][3] = bounds[pos{y, x + 1}][3] + t
-			}
+			currentMax = max(currentMax, v)
 		}
 	}
 
-	for y := 0; y < height; y++ {
-		for x := 0; x < width; x++ {
-			hidden := 1
-			if y > 0 && treeMap[pos{y, x}] > bounds[pos{y - 0, x}][i] {
-				hidden = 0
-			}
-		}
-		sum += hidden
-	}
+	return len(seen)
+}
 
-	return sum
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
 }
 
 func toInt(s string) int {
@@ -93,5 +114,67 @@ func toInt(s string) int {
 }
 
 func part2() int {
-	return 12
+	lines := utils.GetLines(input)
+	treeMap := map[pos]int{}
+	height := len(lines)
+	width := len(lines[0])
+	for y, line := range lines {
+		for x, c := range line {
+			treeMap[pos{y, x}] = toInt(string(c))
+		}
+	}
+	currentMax := 0
+	for p, _ := range treeMap {
+		if p.x == 0 || p.x == width-1 || p.y == 0 || p.y == height-1 {
+			continue
+		}
+		view := getView(treeMap, p, height, width)
+		if view > currentMax {
+			currentMax = view
+		}
+	}
+	return currentMax
+}
+
+func getView(tm map[pos]int, p pos, h, w int) int {
+	overallView := 1
+	treeH := tm[p]
+	// up
+	for y := p.y - 1; y >= 0; y-- {
+		v := tm[pos{y, p.x}]
+		if v >= treeH || y == 0 {
+			overallView *= (p.y - y)
+			break
+		}
+	}
+
+	// down
+	for y := p.y + 1; y < h; y++ {
+		v := tm[pos{y, p.x}]
+		if v >= treeH || y == (h-1) {
+			overallView *= (y - p.y)
+			break
+		}
+	}
+
+	// right
+	for x := p.x + 1; x < w; x++ {
+		v := tm[pos{p.y, x}]
+		if v >= treeH || x == (w-1) {
+			overallView *= (x - p.x)
+			break
+		}
+	}
+
+	// left
+	for x := p.x - 1; x >= 0; x-- {
+		v := tm[pos{p.y, x}]
+		if v >= treeH || x == 0 {
+			overallView *= (p.x - x)
+			break
+		}
+	}
+
+	return overallView
+
 }
